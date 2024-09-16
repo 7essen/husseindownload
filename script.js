f8073dab4609251a3495ce2d27f7a2001278aed1
7essen <116234127+7essen@users.noreply.github.com>
2024-09-16 20:35:43 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    on: {
+        init: function () {
+            var slides = document.querySelectorAll('.swiper-slide');
+            slides.forEach((slide) => {
+                slide.style.opacity = '0'; // إخفاء جميع الشرائح عند التحميل
+            });
+            slides[this.activeIndex].style.opacity = '1'; // إظهار الشريحة النشطة
+        },
+        slideChangeTransitionStart: function () {
+            var slides = document.querySelectorAll('.swiper-slide');
+            slides.forEach((slide) => {
+                slide.style.opacity = '0'; // إخفاء جميع الشرائح
+            });
+            slides[this.activeIndex].style.opacity = '1'; // إظهار الشريحة النشطة
+        }
+    }
+});
