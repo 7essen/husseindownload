e9ce0a493bc246529dc8e3a3da16bb6ced36e6c7
7essen <116234127+7essen@users.noreply.github.com>
2024-09-18 05:16:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// إعداد سلايدر لقطات الشاشة
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1, // عرض شريحة واحدة فقط في كل مرة
     spaceBetween: 0, // لا يوجد تباعد بين الشرائح
@@ -34,4 +35,36 @@ var swiper = new Swiper('.swiper-container', {
             slides[this.activeIndex].style.visibility = 'visible'; // جعل الشريحة النشطة مرئية
         }
     }
+});
+
+// إعداد الرسم البياني باستخدام JSC
+var chart,
+options = { 
+  debug: true, 
+  type: 'column', 
+  title_label_text: 'أداء التطبيقات', 
+  defaultPoint_label_visible: true,
+  
+  series: [{
+      points: [
+          { x: '7eSen TV', y: 90 },
+          { x: 'تطبيق A', y: 70 },
+          { x: 'تطبيق B', y: 60 },
+          { x: 'تطبيق C', y: 50 }
+      ]
+     }],
+     
+     axes: [
+       { 
+         primary: true, 
+         title_label_text: 'التطبيقات' 
+       }, 
+       { 
+         title_label_text: 'النسبة المئوية' 
+       }
+     ]
+}; 
+
+JSC.ready().then(function() { 
+  chart = new JSC.Chart('chartDiv', options); 
 });
